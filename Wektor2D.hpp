c0746fc647ea338fe9473596b3adc962411b62a2
Szymon Ciszewski <szymon526.ciszewski@wp.pl>
2024-10-17 23:09:51 +0200

    Zejściówka 1.

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,36 @@
 class Wektor2D
 {
     // Tutaj napisz implementacje klasy Wektor2D
+public:
+    void   setX(double X) { x = X; }
+    void   setY(double Y) { y = Y; }
+    double getX() { return x; }
+    double getY() { return y; }
+    Wektor2D() {}
+    Wektor2D(double X, double Y)
+    {
+        x = X;
+        y = Y;
+    }
+    Wektor2D(Wektor2D w1, Wektor2D w2)
+    {
+        x = w1.getX() + w2.getX();
+        y = w1.getY() + w2.getY();
+    }
+    double dotp(Wektor2D w1, Wektor2D w2) { 
+        return w1.getX() * w2.getX() + w1.getY() * w2.getY(); 
+    }
+
+    double x;
+    double y;
 };
+
+Wektor2D operator+(Wektor2D w1, Wektor2D w2)
+{
+    return Wektor2D{w1, w2};
+}
+
+double operator*(Wektor2D w1, Wektor2D w2)
+{
+    return w1.dotp(w1, w2);
+}
